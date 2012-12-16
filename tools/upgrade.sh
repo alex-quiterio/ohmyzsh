be22bd48a72c6ff9b6e5ec5c7c105aa01978c346
Alexandre Quitério <a.quiterio@karma-consulting.pt>
2012-12-16 11:57:38 +0000

    all ok

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -1,6 +1,6 @@
-pushd
-	printf '\033[0;34m%s\033[0m\n' "Upgrading Oh My Zsh"
-	cd $ZSH
+current_dir=`pwd`
+printf '\033[0;34m%s\033[0m\n' "Upgrading Oh My Zsh"
+cd $ZSH
 
 	if git pull origin master
 	then
@@ -11,8 +11,8 @@ pushd
 	  printf '\033[0;32m%s\033[0m\n' '\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '
 	  printf '\033[0;32m%s\033[0m\n' '                        /____/                       '
 	  printf '\033[0;34m%s\033[0m\n' 'Hooray! Oh My Zsh has been updated and/or is at the current version.'
-	  printf '\033[0;34m%s\033[1m%s\033[0m\n' 'To keep up on the latest, be sure to follow Oh My Zsh on twitter: ' 'http://twitter.com/ohmyzsh'
 	else
 	  printf '\033[0;31m%s\033[0m\n' 'There was an error updating. Try again later?'
 	fi
-popd
+
+cd $current_dir
