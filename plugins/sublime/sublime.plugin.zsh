864d4e8016ab55a7a43c5bd1e1779c5bac0d540f
Alexandre Quiterio <quiterio7@gmail.com>
2013-01-20 13:29:55 +0000

    Update plugins/sublime/sublime.plugin.zsh

--- plugins/sublime/sublime.plugin.zsh
+++ plugins/sublime/sublime.plugin.zsh
@@ -1,7 +1,6 @@
 install_sublime() {
-
 	wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.tar.bz2
 	tar xvf Sublime\ Text\ 2.0.1.tar.bz2
-	rm Sublime\ Text\ 2.0.1.tar.bz2
-	mv Sublime\Text\ 2 sublime
+	rm *.tar.bz2
+	mv Sublime\ Text\ 2 sublime
 }
