a62650560e0188ea90ca8060a4cfb5af982e637d
alex-quiterio <a.quiterio@karma-consulting.pt>
2013-01-20 13:21:25 +0000

    add sublime pulgin with some install configurations

---
+++ plugins/sublime/sublime.plugin.zsh
@@ -0,0 +1,7 @@
+function install_sublime() {
+
+	wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.tar.bz2
+	tar xvf Sublime\ Text\ 2.0.1.tar.bz2
+	rm Sublime\ Text\ 2.0.1.tar.bz2
+	mv Sublime\Text\ 2 sublime
+}
